eda4c14c29542aef0d4c65bdbf79664660a4db61
VladislavAduenko <adyenko.job@gmail.com>
2024-07-27 18:52:13 +0300

    Add hw-10 regex

--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -1,6 +1,29 @@
 package telran.strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.strings.Strings.*;
+import org.junit.jupiter.api.Test;
+
 public class RegularExpressionsTest {
+@Test
     void javaVariableTest(){
-        //TODO
+        assertTrue("variableName".matches(javaVariable()));
+        assertTrue("$variableName".matches(javaVariable()));
+        assertTrue("_variableName".matches(javaVariable()));
+        assertTrue("variable0Name".matches(javaVariable()));
+        assertTrue("VariableName".matches(javaVariable()));
+        assertTrue("variable".matches(javaVariable()));
+        
+        assertFalse("123variable".matches(javaVariable()));
+        assertFalse("my-Variable".matches(javaVariable()));
+        assertFalse("my Variable".matches(javaVariable()));
+        assertFalse("variableName!".matches(javaVariable()));
+        assertFalse("?variableName!".matches(javaVariable()));
+        assertFalse("int".matches(javaVariable()));
+        assertFalse("for".matches(javaVariable()));
+        assertFalse("@name".matches(javaVariable()));
+        assertFalse("{1}".matches(javaVariable()));
+        assertFalse("".matches(javaVariable()));
+        assertFalse("_".matches(javaVariable()));
     }
 }
