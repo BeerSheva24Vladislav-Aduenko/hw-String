f0b3bd3e76d9c1a1a8026836e61c16fe3379e078
VladislavAduenko <adyenko.job@gmail.com>
2024-07-31 18:21:55 +0300

    class work

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,23 +1,78 @@
 package telran.strings;
 
-public class Strings {
+import java.util.Arrays;
 
-    public static String[] keywords =  {
-        "abstract", "continue", "for", "new", "switch",
-        "assert", "default", "goto", "package", "synchronized",
-        "boolean", "do", "if", "private", "this",
-        "break", "double", "implements", "protected", "throw",
-        "byte", "else", "import", "public", "throws",
-        "case", "enum", "instanceof", "return", "transient",
-        "catch", "extends", "int", "short", "try",
-        "char", "final", "interface", "static", "void",
-        "class", "finally", "long", "strictfp", "volatile",
-        "const", "float", "native", "super", "while"
-    };
-    public static final String JAVA_VARIABLE_PATTERN = 
-        "^(?!(%s)$)(?!_$)[A-Za-z$_][0-9A-Za-z$_]*$";
+public class Strings {
+    static final String keyWords[] = { "abstract", "assert", "boolean",
+                "break", "byte", "case", "catch", "char", "class", "const",
+                "continue", "default", "do", "double", "else", "enum", "extends", "false",
+                "final", "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long", "native",
+                "new", "null", "package", "private", "protected", "public",
+                "return", "short", "static", "strictfp", "super", "switch",
+                "synchronized", "this", "throw", "throws", "transient", "true",
+                "try", "void", "volatile", "while" };
+    public static String firstName() {
+        // regex for strings startin with
+        // capital letter and rest as a
+        // lower case letters
+        // minimal length is 5 letters
+        return "[A-Z][a-z]{4,}";
+    }
 
     public static String javaVariable() {
-        return String.format(JAVA_VARIABLE_PATTERN, String.join("|", keywords));
+        // regular expression for testing synthax
+        // of Java variable names
+        return "((?!_$)[a-zA-Z$_][\\w$]*)";
     }
+    public static String number0_300() {
+       
+        return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+    }
+    public static String ipV4Octet(){
+        
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+    public static String ipV4Address(){
+        String octetExpr = ipV4Octet();
+        return String.format("%s(\\.%s){3}", octetExpr, octetExpr);
+    }
+    public static String stringWithJavaNames(String names) {
+       String [] tokens = names.split("\\s+");
+       int i = getJavaNameIndex(tokens, -1);
+       String res = "";
+       if (i >= 0) {
+         res = tokens[i];
+       }
+       while((i = getJavaNameIndex(tokens, i)) > 0) {
+            res += " " + tokens[i];
+       }
+       
+        return res;
+    }
+
+    private static int getJavaNameIndex(String[] tokens, int i) {
+        i++;
+        while(i < tokens.length && !isJavaName(tokens[i])) {
+            i++;
+        }
+        return i < tokens.length ? i : -1;
+    }
+
+    private static boolean isJavaName(String string) {
+        
+        return string.matches(javaVariable()) && Arrays.binarySearch(keyWords, string) < 0;
+}
+public static boolean isArithmeticExpression(String expr) {
+    //TODO
+    //1. brackets
+    //right position of open / close bracket is matter of regex
+    //matching between open and close bracket is matter of the method you are supposed to write
+    //based on a counter. If counter is negative - no matching;
+    // if at ending up going through a string the counter doesn't equal 0 - no matching
+    //matching may be only in one case: at the ending up of going the counter will be 0
+    // Operator - regular expression for one out of 4 arithemetic operators [*/+-]
+    //Operand may be either Java variable name or number (better any)
+    return false;
+}
 }
--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -1,29 +1,103 @@
 package telran.strings;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static telran.strings.Strings.*;
+
 import org.junit.jupiter.api.Test;
 
 public class RegularExpressionsTest {
-@Test
-    void javaVariableTest(){
-        assertTrue("variableName".matches(javaVariable()));
-        assertTrue("$variableName".matches(javaVariable()));
-        assertTrue("_variableName".matches(javaVariable()));
-        assertTrue("variable0Name".matches(javaVariable()));
-        assertTrue("VariableName".matches(javaVariable()));
-        assertTrue("variable".matches(javaVariable()));
+    @Test
+    void javaVariableTrueTest(){
+        String regex = Strings.javaVariable();
         
-        assertFalse("123variable".matches(javaVariable()));
-        assertFalse("my-Variable".matches(javaVariable()));
-        assertFalse("my Variable".matches(javaVariable()));
-        assertFalse("variableName!".matches(javaVariable()));
-        assertFalse("?variableName!".matches(javaVariable()));
-        assertFalse("int".matches(javaVariable()));
-        assertFalse("for".matches(javaVariable()));
-        assertFalse("@name".matches(javaVariable()));
-        assertFalse("{1}".matches(javaVariable()));
-        assertFalse("".matches(javaVariable()));
-        assertFalse("_".matches(javaVariable()));
+        assertTrue("name".matches(regex));
+        assertTrue("a_1".matches(regex));
+        assertTrue("S".matches(regex));
+        assertTrue("$".matches(regex));
+        assertTrue("__".matches(regex)); //two underscore symbols
+        assertTrue("ab_cd_$12$".matches(regex));
     }
+    @Test
+    void javaVariableFalseTest(){
+        String regex = Strings.javaVariable();
+        
+        assertFalse("1name".matches(regex));
+        assertFalse("1_a".matches(regex));
+        assertFalse("_".matches(regex));
+        assertFalse("#1".matches(regex));
+        assertFalse("_*".matches(regex)); //two underscore symbols
+        assertFalse("ab_cd_$12?".matches(regex));
+        assertFalse("ab_cd_$12 ".matches(regex));
+    }
+    @Test
+    void number0_300TrueTest(){
+        String regex = Strings.number0_300();
+        assertTrue("0".matches(regex));
+        assertTrue("300".matches(regex));
+        assertTrue("250".matches(regex));
+        assertTrue("25".matches(regex));
+        assertTrue("12".matches(regex));
+        assertTrue("299".matches(regex));
+        assertTrue("1".matches(regex));
+
+    }
+    @Test
+    void number0_300FalseTest(){
+        String regex = Strings.number0_300();
+        assertFalse("00".matches(regex));
+        assertFalse("301".matches(regex));
+        assertFalse("01".matches(regex));
+        assertFalse("00".matches(regex));
+        assertFalse("1(".matches(regex));
+        assertFalse("1000".matches(regex));
+        assertFalse(" 20".matches(regex));
+        assertFalse("1001".matches(regex));
+    }
+    @Test
+    void ipV4OctetTrueTest(){
+        String regex = Strings.ipV4Octet();
+        assertTrue("0".matches(regex));
+        assertTrue("00".matches(regex));
+        assertTrue("000".matches(regex));
+        assertTrue("10".matches(regex));
+        assertTrue("100".matches(regex));
+        assertTrue("255".matches(regex));
+        assertTrue("199".matches(regex));
+        assertTrue("249".matches(regex));
+    }
+    @Test
+    void ipV4OctetFalseTest(){
+        String regex = Strings.ipV4Octet();
+        assertFalse("0000".matches(regex));
+        assertFalse("t".matches(regex));
+        assertFalse("-1".matches(regex));
+        assertFalse("1111".matches(regex));
+        assertFalse("0001".matches(regex));
+        assertFalse("256".matches(regex));
+        assertFalse("300".matches(regex));
+        assertFalse("*".matches(regex));
+        assertFalse("1 ".matches(regex));
+    }
+    @Test
+    void ipV4AddressTrueTest() {
+        String regex = Strings.ipV4Address();
+        assertTrue("0.0.0.0".matches(regex));
+        assertTrue("255.255.255.255".matches(regex));
+    }
+    @Test
+    void ipV4AddressFalseTest() {
+        String regex = Strings.ipV4Address();
+        assertFalse("0.0.0".matches(regex));
+        assertFalse("0.0.0+0".matches(regex));
+        assertFalse("0".matches(regex));
+        assertFalse("0.-".matches(regex));
+        assertFalse("0.0.0*0".matches(regex));
+        assertFalse("0.0.0 0".matches(regex));
+    }
+    @Test
+    void stringWithJavaNamesTest() {
+        String names = "123 1a _ abs int enum null lmn";
+        String expected = "abs lmn";
+        assertEquals(expected, Strings.stringWithJavaNames(names));
+    }
+    
 }
